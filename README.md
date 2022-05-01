871437884feb5caab216921c6b799025196d0fca
Bruce <contact@estimatebodyfat.com>
2022-05-01 01:16:40 -0400

    game complete

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Group Class Work #30 of 100 Devs 
 
-Built this Tic Tac Toe game by learning from Kyle of Web Dev Simplified (tutorial): https://www.youtube.com/watch?v=Y-GkMjUZsmM&t=1478&ab_channel=WebDevSimplified
+Built this Tic Tac Toe game based on this tutorial from Web Dev Simplified (tutorial): 
+
+https://www.youtube.com/watch?v=Y-GkMjUZsmM&t=1478&ab_channel=WebDevSimplified
--- js/main.js
+++ js/main.js
@@ -1,13 +1,43 @@
 const gameCell = document.querySelectorAll('[game-cell]')
 const gameBoard = document.getElementById('game-board')
+const whoWonPopUp = document.querySelector('[game-message]')
+const whoWonPopUpMessage = document.querySelector('[who-won]')
+const restartButton = document.querySelector('[restart]')
+
+
 let oTurn
 let X = 'x'
 let O = 'o'
 
- gameCell.forEach(cell => {
-     //Only fire the event listener once 
-     cell.addEventListener('click', handleClick, {once: true})
- })
+const winningCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+
+startGame()
+
+restartButton.addEventListener('click', startGame)
+
+function startGame(){
+    oTurn = false
+    gameCell.forEach(cell => {
+        cell.classList.remove(X)
+        cell.classList.remove(O)
+        cell.removeEventListener('click', handleClick)
+        //Only fire the event listener once 
+        cell.addEventListener('click', handleClick, {once: true})
+    })
+    setBoardHoverClass()
+    whoWonPopUp.classList.remove('show')
+}
+
 
 
  function handleClick(e){
@@ -15,12 +45,48 @@ let O = 'o'
       const currentClass = oTurn ? O : X
       placeMark(clickedCell, currentClass)
       
-
-      swapTurns()
-      //Need the hover class to be based on who's upcoming turn it is
-      setBoardHoverClass()
+      if(checkWinner(currentClass)){
+        console.log("Winner")
+        endGame(false)
+      }else if(isDraw()){
+        console.log("Draw")
+        endGame(true)
+      }
+      else {
+        console.log("Switching Turns")
+        swapTurns()
+        //Need the hover class to be based on who's upcoming turn it is
+        setBoardHoverClass()
+      }
 
  }
+
+ function isDraw(){
+     return [...gameCell].every(cell => {
+       return cell.classList.contains(X) ||cell.classList.contains(O)
+     })
+ }
+
+
+
+ 
+ function endGame(draw){
+    draw ? whoWonPopUpMessage.innerText = `Draw!` : whoWonPopUpMessage.innerText = `${oTurn ? "O's" : "X's"} Wins!`
+    whoWonPopUp.classList.add('show')
+ }
+
+ function checkWinner(currentClass){
+    return winningCombinations.some(combinations => {
+        return combinations.every(index => {
+            return gameCell[index].classList.contains(currentClass)
+
+        })
+    })
+ }
+
+
+
+ 
 
 
  function placeMark(clickedCell, currentClass){
@@ -34,8 +100,7 @@ let O = 'o'
 function setBoardHoverClass(){   
     gameBoard.classList.remove(O)
     gameBoard.classList.remove(X)
-
-    oTurn ? gameBoard.classList.add(O) ? gameBoard.classList.add(X)
+    oTurn ? gameBoard.classList.add(O) : gameBoard.classList.add(X)
 
 }
 
